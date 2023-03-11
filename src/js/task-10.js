e18b1bc3ff1a9f0235c29d4023c350bf8f72996e
raffamieczkowski <raffa.mieczkowski@gmail.com>
2023-03-11 12:52:24 +0100

    done 10
    done 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// pobieramy potrzebne elementy z DOM
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+// funkcja tworząca kolekcję elementów
+function createBoxes(amount) {
+  let size = 30; // początkowy rozmiar elementu
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    boxes.appendChild(div);
+    size += 10; // zwiększamy rozmiar elementu o 10px
+  }
+}
+
+// funkcja usuwająca kolekcję elementów
+function destroyBoxes() {
+  boxes.innerHTML = ''; // usuwamy zawartość div#boxes
+}
+
+// obsługa przycisków
+createBtn.addEventListener('click', () => {
+  const amount = input.value;
+  createBoxes(amount);
+});
+
+destroyBtn.addEventListener('click', () => {
+  destroyBoxes();
+});
