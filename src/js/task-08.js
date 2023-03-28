c4303e7d9782de04b520457c29cae6294d826640
raffamieczkowski <raffa.mieczkowski@gmail.com>
2023-03-28 21:06:11 +0200

    objaśnienia
    objaśnienia

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,24 +1,24 @@
 const loginForm = document.querySelector('.login-form');
 
 loginForm.addEventListener('submit', (event) => {
-  event.preventDefault();
+  event.preventDefault(); // zapobiegamy domyślnej akcji formularza
 
   const emailInput = loginForm.elements.email;
   const passwordInput = loginForm.elements.password;
 
-
+  // Sprawdzamy, czy pola są uzupełnione
   if (emailInput.value === '' || passwordInput.value === '') {
     alert('Wypełnij wszystkie pola formularza!');
-    return;
+    return; // przerywamy działanie funkcji, jeśli pola nie są uzupełnione
   }
 
-
+  // Tworzymy obiekt z danymi z formularza
   const formData = {
     email: emailInput.value,
     password: passwordInput.value,
   };
 
-  console.log(formData);
+  console.log(formData); // wyświetlamy obiekt w konsoli
 
-  loginForm.reset();
+  loginForm.reset(); // czyszczenie formularza
 });
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -10,24 +10,25 @@ const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
 const boxes = document.querySelector('#boxes');
 
+// funkcja tworząca kolekcję elementów
 function createBoxes(amount) {
-  let size = 30;
+  let size = 30; // początkowy rozmiar elementu
   for (let i = 0; i < amount; i++) {
     const div = document.createElement('div');
     div.style.width = `${size}px`;
     div.style.height = `${size}px`;
     div.style.backgroundColor = getRandomHexColor();
     boxes.appendChild(div);
-    size += 10;
+    size += 10; // zwiększamy rozmiar elementu o 10px
   }
 }
 
-
+// funkcja usuwająca kolekcję elementów
 function destroyBoxes() {
-  boxes.innerHTML = '';
+  boxes.innerHTML = ''; // usuwamy zawartość div#boxes
 }
 
-
+// obsługa przycisków
 createBtn.addEventListener('click', () => {
   const amount = input.value;
   createBoxes(amount);
